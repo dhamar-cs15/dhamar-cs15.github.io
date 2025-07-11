d742fb165eddd0f9ce0d312bab9781ae0c2de9d0
Dhamar Carrillo <dhamarcarrillo@college.harvard.edu>
2025-07-10 21:02:45 -0700

    added tab links about me section

--- homepage.js
+++ homepage.js
@@ -87,5 +87,18 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(type, 1000);
 });
 
+// About Me Tablinks
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+}
+
 
 

407d90dfdcece42d1cc1347b6ff42c4787ee84fd
Dhamar Carrillo <dhamarcarrillo@college.harvard.edu>
2025-07-17 23:07:30 -0700

    fixed about me section

--- homepage.js
+++ homepage.js
@@ -91,13 +91,15 @@ document.addEventListener("DOMContentLoaded", () => {
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname) {
+function opentab(event, tabname) {
     for(tablink of tablinks){
         tablink.classList.remove("active-link");
     }
     for(tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");
     }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
 
